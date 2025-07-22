f7e35a199057c7c611137bd2fde47395fef38d8e
Max Chuang <maxcchuang@google.com>
2025-07-22 15:10:12 +0800

    change color

--- lua/marx/highlight.lua
+++ lua/marx/highlight.lua
@@ -5,9 +5,9 @@ M.virt_text_hl = "MarxVirtualTextHL"
 M.code_hl = "MarxCodeHL"
 
 M.setup = function()
-  vim.api.nvim_set_hl(0, M.sign_hl, { fg = "#ff8800" })
-  vim.api.nvim_set_hl(0, M.virt_text_hl, { fg = "#ff8800" })
-  vim.api.nvim_set_hl(0, M.code_hl, { bg = "#402200" })
+  vim.api.nvim_set_hl(0, M.sign_hl, { fg = "#65bcff" })
+  vim.api.nvim_set_hl(0, M.virt_text_hl, { fg = "#65bcff" })
+  vim.api.nvim_set_hl(0, M.code_hl, { bg = "#394b70" })
 end
 
 return M
