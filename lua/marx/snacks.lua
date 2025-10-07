9a86f28cc4740f7a6fa7adac61565db95326c0e3
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-10-08 00:01:49 +0800

    feat: add keybindings in snacks picker to remove a marx

--- lua/marx/snacks.lua
+++ lua/marx/snacks.lua
@@ -2,6 +2,10 @@ local M = {}
 
 local database = require "marx.database"
 
+---@class marx.SnacksItem: snacks.picker.finder.Item
+---@field marx_id number
+
+---@module "snacks"
 ---@type snacks.picker.Config
 M.source = {
   title = "marx",
@@ -17,12 +21,13 @@ M.source = {
         if #title < 40 then
           title = title .. string.rep(" ", 40 - #title)
         end
-        ---@type snacks.picker.finder.Item
+        ---@type marx.SnacksItem
         local item = {
           text = table.concat({ title, filename, path }, " "),
           label = title,
           file = mark.path,
           pos = { mark.row + 1, 0 },
+          marx_id = mark.id,
         }
         return item
       end,
@@ -31,6 +36,27 @@ M.source = {
     return items
   end,
   format = "file",
+  actions = {
+    remove_marx = function(picker)
+      picker.preview:reset()
+      for _, item in ipairs(picker:selected { fallback = true }) do
+        if item.marx_id then
+          database.remove_mark(item.marx_id)
+        end
+      end
+      picker.list:set_selected()
+      picker.list:set_target()
+      picker:find()
+    end,
+  },
+  win = {
+    input = {
+      keys = {
+        ["<c-x>"] = { "remove_marx", mode = { "n", "i" } },
+      },
+    },
+    list = { keys = { ["dd"] = "remove_marx" } },
+  },
 }
 
 return M
