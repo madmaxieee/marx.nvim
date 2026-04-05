25d35aeed0c758a2a0896563064c6e1e8f5354a0
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-04-05 23:18:34 +0800

    fix: update highlights for light themes
    Set highlight colors based on the current background and refresh them
    when the colorscheme changes.

--- lua/marx/highlight.lua
+++ lua/marx/highlight.lua
@@ -4,10 +4,26 @@ M.sign_hl = "MarxSignHL"
 M.virt_text_hl = "MarxVirtualTextHL"
 M.background_hl = "MarxBackgroundHL"
 
+local function setup_hl_groups()
+  if vim.o.background == "light" then
+    vim.api.nvim_set_hl(0, M.sign_hl, { fg = "#179299" })
+    vim.api.nvim_set_hl(0, M.virt_text_hl, { fg = "#179299" })
+    vim.api.nvim_set_hl(0, M.background_hl, { bg = "#caf8fa" })
+  else
+    vim.api.nvim_set_hl(0, M.sign_hl, { fg = "#4fd6be" })
+    vim.api.nvim_set_hl(0, M.virt_text_hl, { fg = "#4fd6be" })
+    vim.api.nvim_set_hl(0, M.background_hl, { bg = "#273849" })
+  end
+end
+
 M.setup = function()
-  vim.api.nvim_set_hl(0, M.sign_hl, { fg = "#4fd6be" })
-  vim.api.nvim_set_hl(0, M.virt_text_hl, { fg = "#4fd6be" })
-  vim.api.nvim_set_hl(0, M.background_hl, { bg = "#273849" })
+  setup_hl_groups()
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    desc = "update colors",
+    callback = function()
+      setup_hl_groups()
+    end,
+  })
 end
 
 return M
